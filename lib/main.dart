c588ffcb7e399a96dc02ca343bceb6182ab6eb37
Piyush <piyush.dongre16@gmail.com>
2020-10-26 17:34:46 +0530

    Starter Project for Fl charts demo.

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:fl_charts/pie_chart.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fl Charts Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PieChartSample1(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: PageView(
+          children: <Widget>[
+            PieChart(
+              PieChartData(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
